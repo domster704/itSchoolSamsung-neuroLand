7e5dd5cd83a14d59a4c575755ed6867c8cb22f38
domster704 <61056244+domster704@users.noreply.github.com>
2021-01-22 14:55:11 +0400

    add menu (at least I tried)

--- app/src/main/java/ru/gisupov/neuroland/main_ui/GPSActivity.java
+++ app/src/main/java/ru/gisupov/neuroland/main_ui/GPSActivity.java
@@ -1,13 +1,21 @@
 package ru.gisupov.neuroland.main_ui;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import ru.gisupov.neuroland.R;
 
-public class GPSActivity extends AppCompatActivity {
+public class GPSActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +23,9 @@ public class GPSActivity extends AppCompatActivity {
         setContentView(R.layout.gps);
     }
 
-    public void goToMap(View view) {
-//        Intent intent = new Intent(this, MapsActivity.class);
-//        startActivity(intent);
-    }
+
+//    public void goToMap(View view) {
+////        Intent intent = new Intent(this, MapsActivity.class);
+////        startActivity(intent);
+//    }
 }
--- app/src/main/java/ru/gisupov/neuroland/main_ui/MainActivity.java
+++ app/src/main/java/ru/gisupov/neuroland/main_ui/MainActivity.java
@@ -1,38 +1,72 @@
 package ru.gisupov.neuroland.main_ui;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Objects;
 
 import ru.gisupov.neuroland.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Fragment {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.activity_main, container, false);
+        return root;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Button ar = (Button) getView().findViewById(R.id.ar_but);
+        ar.setOnClickListener(this::goToAr);
+
+        Button web = (Button) getView().findViewById(R.id.web_but);
+        web.setOnClickListener(this::goToWeb);
+
+        Button gps = (Button) getView().findViewById(R.id.gps_but);
+        gps.setOnClickListener(this::goToGPS);
+
+        Button login = (Button) getView().findViewById(R.id.log_but);
+        login.setOnClickListener(this::goToLogin);
+
     }
 
     public void goToGPS(View view) {
-        Intent intent = new Intent(this, GPSActivity.class);
+        Intent intent = new Intent(getActivity().getApplicationContext(), GPSActivity.class);
         startActivity(intent);
     }
 
     public void goToLogin(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(getActivity().getApplicationContext(), LoginActivity.class);
         startActivity(intent);
     }
 
     public void goToAr(View view) {
-        Intent intent = new Intent(this, ARActivity.class);
+        Intent intent = new Intent(requireActivity().getApplicationContext(), ARActivity.class);
         startActivity(intent);
     }
 
     public void goToWeb(View view) {
-        Intent intent = new Intent (this, WebChooseActivity.class);
+        Intent intent = new Intent (getActivity().getApplicationContext(), WebChooseActivity.class);
         startActivity(intent);
     }
 }
