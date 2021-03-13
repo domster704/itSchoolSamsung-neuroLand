0faf78d2ff372f4be93cefafa61736e4082d6bc6
domster704 <61056244+domster704@users.noreply.github.com>
2021-03-13 18:36:49 +0400

    add possibility of input url and getting price of land (not working)

--- app/src/main/java/ru/gisupov/neuroland/main_ui/WebChooseActivity.java
+++ app/src/main/java/ru/gisupov/neuroland/main_ui/WebChooseActivity.java
@@ -3,6 +3,18 @@ package ru.gisupov.neuroland.main_ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.StringJoiner;
 
 import ru.gisupov.neuroland.R;
 
@@ -12,5 +24,50 @@ public class WebChooseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.web);
+    }
+
+    private static String getHTML(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        try (BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+        }
+        return result.toString();
+    }
+
+    private static String putHTML(String urlToRead, String data) throws Exception {
+        String boundary = Long.toHexString(System.currentTimeMillis());
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setUseCaches(false);
+        conn.setDoOutput(true);
+        conn.setDoInput(true);
+
+        StringJoiner sj = new StringJoiner("&");
+        sj.add(URLEncoder.encode(data, String.valueOf(StandardCharsets.UTF_8)));
+        byte[] out = sj.toString().getBytes(StandardCharsets.UTF_8);
+        int length = out.length;
+
+        conn.setFixedLengthStreamingMode(length);
+        try(OutputStream os = conn.getOutputStream()) {
+            os.write(out);
+        }
+
+        return getHTML(urlToRead);
+    }
+
+    public void getDataFromLink(View view) throws Exception {
+        EditText et = (EditText) findViewById(R.id.textLink);
+        TextView tv = (TextView) findViewById(R.id.textCost);
+
+        String urlData = et.getText().toString();
+        String cost = putHTML("http://localhost:5000/", urlData);
+        tv.setText(cost);
     }
 }
