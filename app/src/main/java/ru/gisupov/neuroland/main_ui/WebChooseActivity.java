38750b286c50bfe75339e46fbdcea568b8a4701c
domster704 <61056244+domster704@users.noreply.github.com>
2021-04-08 15:39:05 +0400

    сделал взиамосвязь с нейронкой и получение цены

--- app/src/main/java/ru/gisupov/neuroland/main_ui/WebChooseActivity.java
+++ app/src/main/java/ru/gisupov/neuroland/main_ui/WebChooseActivity.java
@@ -8,59 +8,52 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.StringJoiner;
+import java.io.IOException;
+
+import okhttp3.*;
 
 import ru.gisupov.neuroland.R;
 
 public class WebChooseActivity extends AppCompatActivity {
 
+    static class HttpService {
+
+        private final OkHttpClient httpClient = new OkHttpClient();
+
+        public HttpService() {}
+
+        private String sendPOST(String url, String data) throws IOException {
+
+            // json formatted data
+            String json = "{\"data\": \"" + data + "\"}";
+
+            // json request body
+            RequestBody body = RequestBody.create(
+                    MediaType.parse("application/json; charset=utf-8"),
+                    json
+            );
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .addHeader("User-Agent", "OkHttp Bot")
+                    .post(body)
+                    .build();
+
+            try (Response response = httpClient.newCall(request).execute()) {
+
+                if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                // Get response body
+                assert response.body() != null;
+                return response.body().string();
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.web);
-    }
-
-    private static String getHTML(String urlToRead) throws Exception {
-        StringBuilder result = new StringBuilder();
-        URL url = new URL(urlToRead);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        try (BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-            String line;
-            while ((line = rd.readLine()) != null) {
-                result.append(line);
-            }
-        }
-        return result.toString();
-    }
-
-    private static String putHTML(String urlToRead, String data) throws Exception {
-        String boundary = Long.toHexString(System.currentTimeMillis());
-        URL url = new URL(urlToRead);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("POST");
-        conn.setUseCaches(false);
-        conn.setDoOutput(true);
-        conn.setDoInput(true);
-
-        StringJoiner sj = new StringJoiner("&");
-        sj.add(URLEncoder.encode(data, String.valueOf(StandardCharsets.UTF_8)));
-        byte[] out = sj.toString().getBytes(StandardCharsets.UTF_8);
-        int length = out.length;
-
-        conn.setFixedLengthStreamingMode(length);
-        try(OutputStream os = conn.getOutputStream()) {
-            os.write(out);
-        }
-
-        return getHTML(urlToRead);
     }
 
    class AsyncRequest extends Thread {
@@ -72,9 +65,9 @@ public class WebChooseActivity extends AppCompatActivity {
             String urlData = et.getText().toString();
             String cost;
 
-//        String cost = putHTML("http://78.85.180.144:8000/", urlData);
             try {
-                cost = getHTML("http://78.85.180.144:8000/");
+                HttpService httpService = new HttpService();
+                cost = httpService.sendPOST("http://100.80.127.91:8000/url", urlData);
                 tv.setText(cost);
             } catch (Exception e) {
                 e.printStackTrace();
@@ -83,7 +76,6 @@ public class WebChooseActivity extends AppCompatActivity {
     }
 
     public void getDataFromLink(View view) throws Exception {
-//        Process p = Runtime.getRuntime().exec("python app/src/main/java/ru/gisupov/neuroland/main_ui/python_neuro/neuroland/server-flask.py");
         AsyncRequest asyncRequest = new AsyncRequest();
         asyncRequest.start();
         Toast.makeText(getApplicationContext(), "Кнопка нажата", Toast.LENGTH_SHORT).show();
